94fd4cd2219ab46629644a1cabfd178e89e13f70
ali kadhim <ali.crazy60@gmail.com>
2024-11-04 18:32:08 +0300

    Add project files.

---
+++ clean my data/Form1.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBoxDrives = New System.Windows.Forms.ComboBox()
+        Me.LabelTotalSize = New System.Windows.Forms.Label()
+        Me.LabelUsedSize = New System.Windows.Forms.Label()
+        Me.LabelFreeSize = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.ButtonCreateFile = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.LabelTime = New System.Windows.Forms.Label()
+        Me.LabelProgressPercentage = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ComboBoxDrives
+        '
+        Me.ComboBoxDrives.BackColor = System.Drawing.Color.FromArgb(CType(CType(29, Byte), Integer), CType(CType(94, Byte), Integer), CType(CType(113, Byte), Integer))
+        Me.ComboBoxDrives.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ComboBoxDrives.ForeColor = System.Drawing.SystemColors.Window
+        Me.ComboBoxDrives.FormattingEnabled = True
+        Me.ComboBoxDrives.Location = New System.Drawing.Point(29, 44)
+        Me.ComboBoxDrives.Name = "ComboBoxDrives"
+        Me.ComboBoxDrives.Size = New System.Drawing.Size(247, 21)
+        Me.ComboBoxDrives.TabIndex = 0
+        '
+        'LabelTotalSize
+        '
+        Me.LabelTotalSize.AutoSize = True
+        Me.LabelTotalSize.Location = New System.Drawing.Point(117, 84)
+        Me.LabelTotalSize.Name = "LabelTotalSize"
+        Me.LabelTotalSize.Size = New System.Drawing.Size(0, 13)
+        Me.LabelTotalSize.TabIndex = 1
+        '
+        'LabelUsedSize
+        '
+        Me.LabelUsedSize.AutoSize = True
+        Me.LabelUsedSize.Location = New System.Drawing.Point(117, 116)
+        Me.LabelUsedSize.Name = "LabelUsedSize"
+        Me.LabelUsedSize.Size = New System.Drawing.Size(0, 13)
+        Me.LabelUsedSize.TabIndex = 2
+        '
+        'LabelFreeSize
+        '
+        Me.LabelFreeSize.AutoSize = True
+        Me.LabelFreeSize.Location = New System.Drawing.Point(117, 148)
+        Me.LabelFreeSize.Name = "LabelFreeSize"
+        Me.LabelFreeSize.Size = New System.Drawing.Size(0, 13)
+        Me.LabelFreeSize.TabIndex = 3
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(29, 180)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(247, 23)
+        Me.ProgressBar1.TabIndex = 4
+        '
+        'ButtonCreateFile
+        '
+        Me.ButtonCreateFile.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.ButtonCreateFile.Location = New System.Drawing.Point(111, 285)
+        Me.ButtonCreateFile.Name = "ButtonCreateFile"
+        Me.ButtonCreateFile.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonCreateFile.TabIndex = 5
+        Me.ButtonCreateFile.Text = "Start"
+        Me.ButtonCreateFile.UseVisualStyleBackColor = True
+        '
+        'BackgroundWorker1
+        '
+        '
+        'LabelTime
+        '
+        Me.LabelTime.AutoSize = True
+        Me.LabelTime.Location = New System.Drawing.Point(26, 236)
+        Me.LabelTime.Name = "LabelTime"
+        Me.LabelTime.Size = New System.Drawing.Size(0, 13)
+        Me.LabelTime.TabIndex = 6
+        '
+        'LabelProgressPercentage
+        '
+        Me.LabelProgressPercentage.AutoSize = True
+        Me.LabelProgressPercentage.BackColor = System.Drawing.Color.FromArgb(CType(CType(230, Byte), Integer), CType(CType(230, Byte), Integer), CType(CType(230, Byte), Integer))
+        Me.LabelProgressPercentage.Location = New System.Drawing.Point(114, 186)
+        Me.LabelProgressPercentage.Name = "LabelProgressPercentage"
+        Me.LabelProgressPercentage.Size = New System.Drawing.Size(0, 13)
+        Me.LabelProgressPercentage.TabIndex = 7
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button1.Location = New System.Drawing.Point(258, 1)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(47, 23)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "X"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(67, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(305, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.LabelProgressPercentage)
+        Me.Controls.Add(Me.LabelTime)
+        Me.Controls.Add(Me.ButtonCreateFile)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.LabelFreeSize)
+        Me.Controls.Add(Me.LabelUsedSize)
+        Me.Controls.Add(Me.LabelTotalSize)
+        Me.Controls.Add(Me.ComboBoxDrives)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBoxDrives As ComboBox
+    Friend WithEvents LabelTotalSize As Label
+    Friend WithEvents LabelUsedSize As Label
+    Friend WithEvents LabelFreeSize As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents ButtonCreateFile As Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents LabelTime As Label
+    Friend WithEvents LabelProgressPercentage As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ clean my data/Form1.vb
@@ -0,0 +1,113 @@
+﻿Imports System.ComponentModel
+Imports System.IO
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+
+Public Class Form1
+    Private WithEvents fileCreationTimer As Timer
+    Private randomFile As FileStream
+    Private randomFilePath As String
+    Private totalFreeSpace As Long
+    Private elapsedSeconds As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        BackgroundWorker1.WorkerReportsProgress = True
+
+        For Each drive As DriveInfo In DriveInfo.GetDrives()
+            If drive.IsReady Then
+                ComboBoxDrives.Items.Add(drive.Name)
+            End If
+        Next
+    End Sub
+
+    Private Sub ComboBoxDrives_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBoxDrives.SelectedIndexChanged
+        Dim selectedDrive As DriveInfo = New DriveInfo(ComboBoxDrives.SelectedItem.ToString())
+
+        LabelTotalSize.Text = $"Total Size: {FormatBytes(selectedDrive.TotalSize)}"
+        LabelUsedSize.Text = $"Used Space: {FormatBytes(selectedDrive.TotalSize - selectedDrive.AvailableFreeSpace)}"
+        LabelFreeSize.Text = $"Free Space: {FormatBytes(selectedDrive.AvailableFreeSpace)}"
+
+        totalFreeSpace = selectedDrive.AvailableFreeSpace
+        ProgressBar1.Value = 0
+        LabelProgressPercentage.Text = "Progress: 0%"
+    End Sub
+
+    Private Sub ButtonCreateFile_Click(sender As Object, e As EventArgs) Handles ButtonCreateFile.Click
+        If totalFreeSpace <= 0 Then
+            MessageBox.Show("No free space available!")
+            Return
+        End If
+
+        randomFilePath = Path.Combine(ComboBoxDrives.SelectedItem.ToString(), "random_file.tmp")
+        randomFile = New FileStream(randomFilePath, FileMode.Create, FileAccess.Write, FileShare.None)
+
+        fileCreationTimer = New Timer With {.Interval = 1000}
+        fileCreationTimer.Start()
+        elapsedSeconds = 0
+        BackgroundWorker1.RunWorkerAsync()
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Dim buffer(1048576) As Byte
+        Dim bytesWritten As Long = 0
+        Dim progress As Integer
+
+        While bytesWritten < totalFreeSpace
+            If BackgroundWorker1.CancellationPending Then Exit While
+
+            randomFile.Write(buffer, 0, buffer.Length)
+            bytesWritten += buffer.Length
+            progress = CInt((bytesWritten / totalFreeSpace) * 100)
+
+            BackgroundWorker1.ReportProgress(progress, bytesWritten)
+        End While
+
+        randomFile.Close()
+    End Sub
+
+    Private Sub BackgroundWorker1_ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+        ProgressBar1.Value = e.ProgressPercentage
+        LabelProgressPercentage.Text = $"Progress: {e.ProgressPercentage}%"
+
+        Dim bytesWritten As Long = CLng(e.UserState)
+        Dim estimatedTotalSeconds As Double = (elapsedSeconds / bytesWritten) * totalFreeSpace
+        Dim remainingSeconds As Integer = CInt(estimatedTotalSeconds - elapsedSeconds)
+
+        LabelTime.Text = $"Estimated Time Remaining: {FormatTime(remainingSeconds)}"
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        fileCreationTimer.Stop()
+        MessageBox.Show("File creation completed. Do you want to delete the file?", "Completion", MessageBoxButtons.YesNo)
+        If DialogResult.Yes = MessageBox.Show("Delete the created file?", "Confirmation", MessageBoxButtons.YesNo) Then
+            File.Delete(randomFilePath)
+        End If
+    End Sub
+
+    Private Sub FileCreationTimer_Tick(sender As Object, e As EventArgs) Handles fileCreationTimer.Tick
+        elapsedSeconds += 1
+    End Sub
+
+    Private Function FormatBytes(bytes As Long) As String
+        Dim units As String() = {"B", "KB", "MB", "GB", "TB"}
+        Dim size As Double = bytes
+        Dim unitIndex As Integer = 0
+
+        While size >= 1024 AndAlso unitIndex < units.Length - 1
+            size /= 1024
+            unitIndex += 1
+        End While
+
+        Return $"{Math.Round(size, 2)} {units(unitIndex)}"
+    End Function
+
+    Private Function FormatTime(seconds As Integer) As String
+        Dim ts As TimeSpan = TimeSpan.FromSeconds(seconds)
+        Return $"{ts.Hours:D2}:{ts.Minutes:D2}:{ts.Seconds:D2}"
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        End
+    End Sub
+End Class
+
